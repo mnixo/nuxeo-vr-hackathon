0e55e0740af15dae6905c586e6b493d46c3bd023
Miguel Nixo <mnixo@nuxeo.com>
2017-01-16 23:02:13 +0000

    add Miniature

---
+++ Assets/Scripts/MiniatureController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniatureController : MonoBehaviour {
+
+    Text text;
+    Image image;
+    GazeTriggerController trigger;
+
+	void Start() {
+        text = transform.Find("MiniatureHeader/MiniatureTitleCanvas/MiniatureTitleText").GetComponent<Text>();
+        image = transform.Find("MiniatureIconCanvas/MiniatureIconImage").GetComponent<Image>();
+        image.sprite = Resources.Load<Sprite>("Icons/domain");
+        trigger = transform.Find("MiniatureGazeTrigger").GetComponent<GazeTriggerController>();
+        trigger.setCallbacks(onTriggerEnter, onTriggerComplete, onTriggerExit);
+	}
+
+    void onTriggerEnter() {
+        Debug.Log("onTriggerEnter");
+    }
+
+    void onTriggerComplete() {
+        Debug.Log("onTriggerComplete");
+    }
+
+    void onTriggerExit() {
+        Debug.Log("onTriggerExit");
+    }
+
+}
