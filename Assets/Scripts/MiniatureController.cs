ccb19e049a5fe175cf1cecbe77f7340df7c375f5
Miguel Nixo <mnixo@nuxeo.com>
2017-01-17 23:43:12 +0000

    add Miniature generation in MiniatureExplorer

--- Assets/Scripts/MiniatureController.cs
+++ Assets/Scripts/MiniatureController.cs
@@ -7,26 +7,50 @@ public class MiniatureController : MonoBehaviour {
 
     Text text;
     Image image;
+    MiniatureExplorerController explorer;
     GazeTriggerController trigger;
+    string url;
 
-	void Start() {
+	void Awake() {
         text = transform.Find("MiniatureHeader/MiniatureTitleCanvas/MiniatureTitleText").GetComponent<Text>();
         image = transform.Find("MiniatureIconCanvas/MiniatureIconImage").GetComponent<Image>();
-        image.sprite = Resources.Load<Sprite>("Icons/domain");
         trigger = transform.Find("MiniatureGazeTrigger").GetComponent<GazeTriggerController>();
         trigger.setCallbacks(onTriggerEnter, onTriggerComplete, onTriggerExit);
+        setText("(TEXT)");
+        setImage(null);
 	}
 
+    public void setText(string val) {
+        text.text = val;
+    }
+
+    public void setImage(string val) {
+        image.enabled = val != null;
+        image.sprite = val != null ? Resources.Load<Sprite>("Icons/" + val) : null;
+    }
+
+    public void setExplorer(MiniatureExplorerController explorer) {
+        this.explorer = explorer;
+    }
+
+    public string getUrl() {
+        return url;
+    }
+
+    public void setUrl(string val) {
+        url = val;
+    }
+
     void onTriggerEnter() {
-        Debug.Log("onTriggerEnter");
+        
     }
 
     void onTriggerComplete() {
-        Debug.Log("onTriggerComplete");
+        explorer.triggerMiniature(this);
     }
 
     void onTriggerExit() {
-        Debug.Log("onTriggerExit");
+        
     }
 
 }
--- Assets/Scripts/MiniatureExplorerController.cs
+++ Assets/Scripts/MiniatureExplorerController.cs
@@ -6,37 +6,61 @@ public class MiniatureExplorerController : MonoBehaviour {
 
     public GameObject miniaturePrefab;
 
+    float distance = 4.5f;
+    float vAngleIncrement = 16.0f;
+    float hAngleIncrement = 17.0f;
+    float vAngleInitial = -8.0f;
+    int rows = 3;
+    int columns = 2;
+    string baseUrl = "https://nightly.nuxeo.com/";
+    string apiPathRoot = "nuxeo/api/v1/path/";
+    GameObject current;
+    List<GameObject> children;
+
 	void Start() {
-        float distance = 4.5f;
-        float vAngleIncrement = 18.0f;
-        float hAngleIncrement = 20.0f;
-        float vAngleInital = -8.0f;
-        int rows = 3;
-        int columns = 2;
+        current = Instantiate(miniaturePrefab);
+        current.transform.parent = transform;
+        current.transform.localPosition = new Vector3(0.0f, 0.0f, distance);
+        current.transform.RotateAround(Vector3.zero, Vector3.left, rows * vAngleIncrement + vAngleInitial);
+        current.GetComponent<MiniatureController>().setText("ROOT");
+        current.GetComponent<MiniatureController>().setImage("domain");
+        current.GetComponent<MiniatureController>().setExplorer(this);
+        current.GetComponent<MiniatureController>().setUrl(baseUrl + apiPathRoot);
+        children = new List<GameObject>();
         GameObject obj;
-
-        //hAngle = hAngleIncrement * 0;
-
-        /*for (float hAngle = -hAngleIncrement * 2; hAngle < hAngleIncrement * 3; hAngle += hAngleIncrement) {
-            for (float vAngle = 0.0f; vAngle < vAngleIncrement * 3; vAngle += vAngleIncrement) {
-                obj = Instantiate(miniaturePrefab);
-                obj.transform.parent = transform;
-                obj.transform.localPosition = new Vector3(0.0f, Mathf.Sin(vAngle) * distance, Mathf.Cos(vAngle) * distance);
-                obj.transform.eulerAngles = new Vector3(-vAngle * Mathf.Rad2Deg, 0.0f, 0.0f);
-                obj.transform.RotateAround(Vector3.zero, Vector3.up, hAngle * Mathf.Rad2Deg);
-            }
-        }*/
-
         for (int r = rows - 1; r >= 0; r--) {
             for (int c = -columns; c <= columns; c++) {
                 obj = Instantiate(miniaturePrefab);
                 obj.transform.parent = transform;
                 obj.transform.localPosition = new Vector3(0.0f, 0.0f, distance);
-                obj.transform.RotateAround(Vector3.zero, Vector3.left, r * vAngleIncrement + vAngleInital);
+                obj.transform.RotateAround(Vector3.zero, Vector3.left, r * vAngleIncrement + vAngleInitial);
                 obj.transform.RotateAround(Vector3.zero, Vector3.up, c * hAngleIncrement);
+                obj.GetComponent<MiniatureController>().setText("");
+                obj.GetComponent<MiniatureController>().setImage(null);
+                obj.GetComponent<MiniatureController>().setExplorer(this);
+                children.Add(obj);
             }
         }
-
 	}
+
+    IEnumerator WaitForRequest(WWW www) {
+        yield return www;
+        if (www.error == null) {
+            Debug.Log(www.text);
+        } else {
+            Debug.Log(www.error);
+        }
+    }
+
+    void makeRequest(string url) {
+        Dictionary<string, string> headers = new Dictionary<string, string>();
+        headers.Add("Authorization", "Basic " + System.Convert.ToBase64String(
+            System.Text.Encoding.ASCII.GetBytes("Administrator:Administrator")));
+        StartCoroutine(WaitForRequest(new WWW(url, null, headers)));
+    }
+
+    public void triggerMiniature(MiniatureController miniature) {
+        makeRequest(miniature.getUrl());
+    }
 	
 }
